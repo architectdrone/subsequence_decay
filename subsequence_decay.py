be3082ff2afb4bc1998128896b420f0a5e67aefd
Owen Mellema <owen.mellema@cerner.com>
2022-06-04 14:23:02 -0500

    cleanup

--- subsequence_decay.py
+++ subsequence_decay.py
@@ -3,28 +3,6 @@ from math import ceil, floor
 import tabulate
 
 def decay(input):
-    for i in range(len(input)):
-        if i == len(input)-1:
-            return input
-        current = input[i]
-        next = input[i+1]
-
-        if next == 1:
-            continue
-        elif current > next:
-            print("ERROR!")
-            return input
-        elif current == next:
-            continue
-        elif next == current + 1:
-            continue
-        else:
-            
-            input[i] = current + 1
-            input[i+1] = next - 1
-            return input
-
-def decay2(input):
     for i_n in range(len(input)):
         i = len(input) -1 - i_n
         for j_n in range(len(input)-i):
@@ -48,7 +26,7 @@ def get_decay_number(number_of_ones, righthand_side, should_print=False):
     while current != previous:
         
         previous = current.copy()
-        current = decay2(current)
+        current = decay(current)
         if current == previous:
             break
         if should_print:
@@ -56,231 +34,165 @@ def get_decay_number(number_of_ones, righthand_side, should_print=False):
         count+=1
     return count
 
-def total_decay(n):
-    count = 0
-    for i in range(n):
-        decay_amount = get_decay_number(i, n-i, should_print=True)
-        print(i, decay_amount)
-        count+=decay_amount
-    print(count)
-    return count
+class SubsequenceDecay:
+    '''
+    Decay single.
+    '''
+    def decay(input):
+        for i_n in range(len(input)):
+            i = len(input) -1 - i_n
+            for j_n in range(len(input)-i):
+                j = j_n + i
+                if j <= i:
+                    continue
+                current = input[i]
+                next = input[j]
+                if next > current + 1:
+                    input[i] = current + 1
+                    input[j] = next - 1
+                    return input
+        return input
+    
+    def get_initial_state(i, r):
+        return [1 for i in range(i)] + [r]
 
-def decay_sweep(n):
-    previous = 0
-    for i in range(n):
+    def decay_fully(i, r):
+        states = []
+        current = SubsequenceDecay.get_initial_state(i, r)
+        states.append(current.copy())
+        previous = None
+        while current != previous:
+            previous = current.copy()
+            current = SubsequenceDecay.decay(current)
+            if current == previous:
+                break
+            states.append(current.copy())
+        return states
+
+    def get_decay_number(i, r):
+        return len(SubsequenceDecay.decay_fully(i, r))
+
+    def get_fully_decayed_state(i, r):
+        return SubsequenceDecay.decay_fully(i, r)[-1]
+
+    def get_new_states(i, r):
+        return [[1]+SubsequenceDecay.get_fully_decayed_state(i-1, r)] + [x for x in SubsequenceDecay.decay_fully(i, r) if x[0] != 1]
+
+class F_II:
+    def actual(i, r):
+        return F_I.actual(i, r) - F_I.actual(i, r-1)
+
+    def estimated(i, r):
+        if (r < i + 2):
+            return 0
+        if (r == i + 2):
+            return 1
         
-        decay_number = get_decay_number(i, n, should_print=False)
-        print(i, decay_number, decay_number-previous)
-        previous = decay_number
+        if (i == 1):
+            return r%2
+        c = 0
+        if ((r+1)%(i+1) < 3):
+            c = 1
         
-
-def super_decay_sweep(n):
-    lines = []
-    lines.append([""]+[f"r={y}" for y in range(n)])
-    for x in range(n):
-        line = [f"n1={x}"]
-        for y in range(n):
-            line.append(str(get_decay_number(x, y)))
-        lines.append(line)
-    print(tabulate.tabulate(lines))
-
-def super_decay_sweep2(n):
-    lines = []
-    lines.append([""]+[f"r={y}" for y in range(n+1)])
-    for x in range(n+1):
-        line = [f"n1={x}"]
-        for y in range(n+1):
-            line.append(str(get_decay_number(x, y) - get_decay_number(x-1, y)))
-        lines.append(line)
-    print(tabulate.tabulate(lines))
-
-def f(i, r):
-    return get_decay_number(i, r)
-
-def f_i(i, r):
-    return f(i, r) - f(i-1, r)
-
-def f_ii(i, r):
-    return f_i(i, r) - f_i(i, r-1)
-
-def super_decay_sweep3(n):
-    lines = []
-    print("".join(["  "]+[f"{y%10} " for y in range(n+1)]))
-    for x in range(n+1):
-        line = [f"{x%10}"]
-        for y in range(n+1):
-            #number = (get_decay_number(x, y) - get_decay_number(x-1, y))-(get_decay_number(x, y-1) - get_decay_number(x-1, y-1))
-            number = f_ii(x, y)
-            to_print = ""
-            if number == 1:
-                to_print = "🟩"
-            elif number == -1:
-                to_print = "🟥"
-            else:
-                to_print = "⬜"
-            line.append(to_print)
-        #print("".join(line))
-        lines.append("".join(line))
-    print("\n".join(lines))
-    #print(tabulate.tabulate(lines))
-
-def get_increase_from_n1_increment(low_n1, high_n1, high_r):
-    for r in range(high_r):
-        low_d = get_decay_number(low_n1, r)
-        high_d = get_decay_number(high_n1, r)
-        print(f"{r} => {high_d - low_d}")
-
-def test_on_r_range(n1, h_r, func):
-    for r in range(h_r):
-        expected = get_decay_number(n1, r)
-        actual = func(r)
-        if (expected == actual):
-            print(f"{r} => ✅")
-        else:
-            print(f"{r} => ❌ (expected: {expected} actual: {actual})")
-
-def generalized_r_scan_for_f_ii(i, func):
-    for r in range(200):
-        n = func(i, r)
-        to_print = ""
-        if n == 1:
-            to_print = "O"
-        elif n == -1:
-            to_print = "X"
-        else:
-            to_print = "_"
-        print(to_print, end="")
-    print()
-
-def f_ii_analysis(i):
-    generalized_r_scan_for_f_ii(i, f_ii)
-
-def f_ii_estimation(i, r):
-    if (r < i + 2):
-        return 0
-    if (r == i + 2):
-        return 1
+        if ((r+i-1)%i == 0):
+            c -= 1
+        return c
     
-    if (i == 1):
-        return r%2
+    '''
+    Gets the group number.
+    '''
+    def group(i, r):
+        return floor((r+1)/(i+1))
 
-    c = 0
+    '''
+    Position within group.
+    '''
+    def s(i, r):
+        return ((r+1)%(i+1))
 
-    if ((r+1)%(i+1) < 3):
-        c = 1
+class F_I:
+    def actual(i, r):
+        return F.actual(i, r) - F.actual(i-1, r)
+
+    def integrated(i, r):
+        count = 0
+        for x in range(r+1):
+            count+=F_II.actual(i, x)
+        return count
     
-    if ((r+i-1)%i == 0):
-        c -= 1
+    '''
+    This is a simplified form of the estimated integral function
+    '''
+    def estimated(i, r):
+        if (F_I._adjusted_r(i, r) < 0):
+            return 0
+        
+        if (i == 1):
+            return ceil(r/2)-1
+
+        term_1 = (3*floor((r+2)/(i+1)))
+        term_2 = min(3, (r+2)%(i+1))
+        term_3 = -1*floor((r-1)/i)
+        term_4 = -4
+        return term_1 + term_2 +term_3 +term_4
+
+    '''
+    Return integral evaluated only when f_ii is positive or zero
+    '''
+    def integral_positive(i, r):
+        return F_I._filtered_integral(i, r, lambda a : a >= 0)
+
+    '''
+    Return integral evaluated only when f_ii is positive or zero
+    '''
+    def integral_negative(i, r):
+        return F_I._filtered_integral(i, r, lambda a : a <= 0)
+
+    '''
+    Estimates the positive component of the integral.
+    '''
+    def estimated_positive_integral(i, r):
+        s = F_II.s(i, r+1)
+        return ((3*(F_II.group(i,r+1)-2))) + min(3, s)
+
+    '''
+    Estimates the negative component of the integral.
+    '''
+    def estimated_negative_integral(i, r):
+        return -1*(floor((F_I._adjusted_r(i, r)+1)/(i))-1)
+
+    '''
+    Sums the negative and positive components.
+    '''
+    def estimated_integral(i, r):
+        if (F_I._adjusted_r(i, r) < 0):
+            return 0
+        
+        if (i == 1):
+            return ceil(r/2)-1
+        return F_I.estimated_positive_integral(i, r) + F_I.estimated_negative_integral(i, r)
+
+    def _filtered_integral(i, r, filter):
+        count = 0
+        for x in range(r+1):
+            n = F_II.actual(i, x)
+            if (filter(n)):
+                count+=F_II.actual(i, x)
+        return count
+
+    def _adjusted_r(i, r):
+        return r-i-2
+
+class F:
+    def actual(i, r):
+        return SubsequenceDecay.get_decay_number(i, r)
     
-    return c
-
-def f_i_by_non_estimated_integration(i, r):
-    count = 0
-    for x in range(r+1):
-        count+=f_ii(i, x)
-    return count
-
-def f_i_by_non_estimated_integration_pos(i, r):
-    count = 0
-    for x in range(r+1):
-        n = f_ii(i, x)
-        if (n >= 0):
-            count+=f_ii(i, x)
-    return count
-
-def f_i_by_non_estimated_integration_neg(i, r):
-    count = 0
-    for x in range(r+1):
-        n = f_ii(i, x)
-        if (n <= 0):
-            count+=f_ii(i, x)
-    return count
-
-def f_i_estimation(i, r):
-    adjusted_r = r-i-2
-    if (f_i_adjusted_r(i, r) < 0):
-        return 0
-    
-    if (i == 1):
-        return ceil(r/2)-1
-    return f_i_positive(i, r) - f_i_negs(i, r)
-
-def f_i_estimation_simplified(i, r):
-    if (f_i_adjusted_r(i, r) < 0):
-        return 0
-    
-    if (i == 1):
-        return ceil(r/2)-1
-
-    term_1 = (3*floor((r+2)/(i+1)))
-    term_2 = min(3, (r+2)%(i+1))
-    term_3 = -1*floor((r-1)/i)
-    term_4 = -4
-    return term_1 + term_2 +term_3 +term_4
-
-def f_i_adjusted_r(i, r):
-    return r-i-2
-
-def f_i_threes(i, r):
-    return floor((f_i_adjusted_r(i, r)-1)/(i+1))
-
-def f_i_positive(i, r):
-    s = f_ii_get_s(i, r+1)
-    return ((3*(f_ii_get_group(i,r+1)-2))) + min(3, s)
-
-def f_i_negs(i, r):
-    return floor((f_i_adjusted_r(i, r)+1)/(i))-1
-
-def compare_f_i(i):
-    expected_list = ["f_i"]
-    actual_list = ["f_i_estimation"]
-    estimated_positives_list = ['f_i_estimation_pos']
-    estimated_negatives_list = ['f_i_estimation_neg']
-    integration_list = ["f_i_by_integration"]
-    positive_integration_list = ["f_i_by_integration_pos"]
-    negative_integration_list = ["f_i_by_integration_neg"]
-
-    for x_i in range(20):
-        x = x_i 
-        expected_list.append(f_i(i, x))
-        actual_list.append(f_i_estimation(i, x))
-        estimated_positives_list.append(f_i_positive(i, x))
-        estimated_negatives_list.append(f_i_negs(i, x)*-1)
-        integration_list.append(str(f_i_by_non_estimated_integration(i, x)))
-        positive_integration_list.append(str(f_i_by_non_estimated_integration_pos(i, x)))
-        negative_integration_list.append(str(f_i_by_non_estimated_integration_neg(i, x)))
-    print(tabulate.tabulate([expected_list, actual_list, integration_list, positive_integration_list, estimated_positives_list, negative_integration_list, estimated_negatives_list]))
-
-def f_ii_get_group(i, r):
-    return floor((r+1)/(i+1))
-
-def f_ii_get_d(i, r):
-    return (i)-((f_ii_get_group(i, r)-3) % (i)) - 1
-
-def f_ii_get_s(i, r):
-    return ((r+1)%(i+1))
-
-def compare_f_ii_estimation(i):
-    generalized_r_scan_for_f_ii(i, f_ii)
-    generalized_r_scan_for_f_ii(i, f_ii_estimation)
-    for r in range(200):
-        adjusted_r = r-i-2
-        negs = floor((adjusted_r+1)/(i))
-        print(f"{negs%10}", end="")
-    print()
-    for r in range(200):
-        adjusted_r = r-i-2
-        threes = floor((adjusted_r-1)/(i+1))
-        print(f"{threes%10}", end="")
-    print()
-    for r in range(200):
-        print(f"{f_ii_get_d(i, r)}", end="")
-    print()
-    for r in range(200):
-        if f_ii_get_d(i, r) == f_ii_get_s(i, r) or (f_ii_get_d(i, r) == 0 and f_ii_get_s(i, r) == 7):
-            print("^", end="")
-        else:
-            print("_", end="")
+    def integrated(i, r):
+        count = ceil(r/2)
+        for x_ in range(i+1):
+            x = x_ +1
+            count += F_I.actual(x, r)
+        return count
 
 def range_test(expected_function, actual_function, test_scope):
     for i_ in range(test_scope):
@@ -290,33 +202,45 @@ def range_test(expected_function, actual_function, test_scope):
             if expected_function(i, r) != actual_function(i, r):
                 print(f"FAILED. i = {i}, r = {r}, expected = {expected_function(i, r)}, actual = {actual_function(i, r)}")
 
-INITIAL = [1,1,1,17]
+def generalized_sweep(function, low_i, high_i, low_r, high_r):
+    lines = []
+    for i_ in range(high_i - low_i):
+        i = i_ + low_i
+        line = []
+        for r_ in range(high_r - low_r):
+            r = r_ + low_r
+            line.append(function(i, r))
+        lines.append(line)
+    print(tabulate.tabulate(lines))
 
-#print(get_decay_number(3, 17))
+def terminal():
+    while True:
+        command = input("> ").split(" ")
+        if command[0] == "e":
+            i = int(command[1])
+            r = int(command[2])
+            get_decay_number(i, r)
+            print(SubsequenceDecay.get_new_states(i, r))
+            print(f"I = {i} R = {r} F = {F.actual(i, r)} F' = {F_I.estimated(i, r)} F'' = {F_II.estimated(i, r)}")
+        if command[0] == "test":
+            print("===== F_II =====")
+            print("Estimated = Actual")
+            range_test(F_II.actual, F_II.estimated, 50)
+            print("===== F_I =====")
+            print("Estimated = Actual")
+            range_test(F_I.actual, F_I.estimated, 50)
+            print("Integrated = Actual")
+            range_test(F_I.actual, F_I.integrated, 50)
+            print("Estimated Integral = Actual")
+            range_test(F_I.actual, F_I.estimated_integral, 50)
+            print("===== F =====")
+            print("Nothing to test.")
+        elif command[0] == "c": 
+            i_1 = int(command[1])
+            r_1 = int(command[2])
+            i_2 = int(command[1])
+            r_2 = int(command[2])
 
-#total_decay(20)
-#decay_sweep(20)
-#super_decay_sweep(40)
-r_when_n1_is_1 = lambda r: ceil(r/2)
-r_when_n1_is_2 = lambda r: r_when_n1_is_1(r) + floor((r/2)-1)
-#test_on_r_range(2, 50, r_when_n1_is_2)
-
-#get_increase_from_n1_increment(3, 4, 50)
-#super_decay_sweep3(40)
-#f_ii_analysis(6)
-#compare_f_i(7)
-
-range_test(f_i, f_i_estimation_simplified, 40)
-
-# I_TO_CHECK = 5
-# for r_high in range(20):
-#     if f_ii_estimation(I_TO_CHECK, r_high) == -1:
-#         print(f"f_ii = {f_ii_estimation(I_TO_CHECK, r_high)}")
-#         r_low_delta = get_decay_number(I_TO_CHECK, r_high-1, should_print=True) - get_decay_number(I_TO_CHECK-1, r_high-1, should_print=True)
-#         r_high_delta = get_decay_number(I_TO_CHECK, r_high  , should_print=True) - get_decay_number(I_TO_CHECK-1, r_high  , should_print=True)
-
-#         print(f"effect of increasing I from {I_TO_CHECK-1} to {I_TO_CHECK} in R={r_high-1} = {r_low_delta}")
-#         print(f"effect of increasing I from {I_TO_CHECK-1} to {I_TO_CHECK} in R={r_high} = {r_high_delta}")
-        
+terminal()
 
 
